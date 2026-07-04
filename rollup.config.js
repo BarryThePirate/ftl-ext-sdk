42cd3c965d5a71e3a78b5f51b6aa44bdd88ce8f9
gazelle <zhanle9090950@163.com>
2026-07-04 20:53:58 +0800

    Add Tampermonkey userscript support

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,26 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
 
+function firefoxUserscriptArrayBufferFix() {
+  const arrayBufferInstanceof = /\b([A-Za-z_$][\w$]*(?:\.[A-Za-z_$][\w$]*)?) instanceof ArrayBuffer/g;
+  const helper = `function __ftlIsArrayBuffer(value) {
+  return Object.prototype.toString.call(value) === '[object ArrayBuffer]';
+}
+`;
+
+  return {
+    name: 'firefox-userscript-arraybuffer-fix',
+    renderChunk(code) {
+      const patched = code.replace(arrayBufferInstanceof, '__ftlIsArrayBuffer($1)');
+      if (patched === code) return null;
+      return {
+        code: helper + patched,
+        map: null,
+      };
+    },
+  };
+}
+
 export default {
   input: 'src/index.js',
   output: [
@@ -22,5 +42,6 @@ export default {
   plugins: [
     resolve({ browser: true }),
     commonjs(),
+    firefoxUserscriptArrayBufferFix(),
   ],
 };
